aed7d7f448f7b0a89f9b441c46908dfe2c6f4789
duc12367 <ducvodich678@gmail.com>
2026-03-18 09:54:13 +0700

    first commit

--- Models/Role.cs
+++ Models/Role.cs
@@ -6,7 +6,7 @@ namespace MilkStore.Models;
 public partial class Role
 {
     public int Id { get; set; }
-
+    
     public string RoleName { get; set; } = null!;
 
     public virtual ICollection<User> Users { get; set; } = new List<User>();
